0a54d9989da0c942b22bd0c5d1240ba0f091240d
brtostes <103864780+brtostes@users.noreply.github.com>
2026-03-24 12:42:49 -0300

    Delete q4_evolucao_mensal_gasto_total.sql

--- q4_evolucao_mensal_gasto_total.sql
+++
@@ -1,7 +0,0 @@
-SELECT d.year,
-       d.month,
-       ROUND(SUM(f.amount_spent), 2) AS total_spent
-FROM lab01.fact_customer_spending f
-JOIN lab01.dim_date d ON f.date_id = d.date_id
-GROUP BY d.year, d.month
-ORDER BY d.year, d.month;
