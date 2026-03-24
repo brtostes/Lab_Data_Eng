c6bfb3f947b5a11188e3a0892ac1c99d4ba3f1d1
brtostes <103864780+brtostes@users.noreply.github.com>
2026-03-24 12:40:48 -0300

    Delete Q3_ticket_medio_por_pg.sql

--- Q3_ticket_medio_por_pg.sql
+++
@@ -1,6 +0,0 @@
-SELECT p.payment_method,
-       ROUND(AVG(f.amount_spent), 2) AS avg_ticket
-FROM lab01.fact_customer_spending f
-JOIN lab01.dim_payment p ON f.payment_id = p.payment_id
-GROUP BY p.payment_method
-ORDER BY avg_ticket DESC;
